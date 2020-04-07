098a317bd16939d65825d8124b2bd59ca26dbadc
Jip de Beer <jip.de.beer@gmail.com>
2020-04-07 10:27:59 +0200

    Override which streams should be high res.

--- extension/src/inject/jitsiFrame.js
+++ extension/src/inject/jitsiFrame.js
@@ -2,29 +2,85 @@
 // Be careful with the scope and API here,
 // use try catch
 
-(() => {
+// Override some styles, these selectors may stop working in the future...
+// Fortunately these styles aren't mission critical
+const style = document.createElement("style");
+document.head.appendChild(style);
+style.sheet.insertRule(
+  "body, .tOoji, #largeVideoContainer, .tile-view #largeVideoContainer {background: transparent !important; background-color: transparent !important;}"
+);
+
+window.addEventListener("load", event => {
   var selectedParticipants = new Set();
   const bc = new BroadcastChannel("popout_jitsi_channel");
-  // TODO: maxVideoHeightToReceive should be based on the resolution
+  // TODO: maxVideoHeightToReceive could be based on the resolution
   // values defined in jitsiConfig.js (listen for a message on the
   // BroadcastChannel)
-  var maxVideoHeightToReceive = 1080;
+  const maxVideoHeightToReceive = 1080;
 
   // Check if 2 sets contain the same values
-  function eqSet(as, bs) {
+  const eqSet = (as, bs) => {
     if (as.size !== bs.size) return false;
     for (var a of as) if (!bs.has(a)) return false;
     return true;
-  }
+  };
+
+  const generateUnusedKey = (suggestedKey, object) => {
+    var i = 0;
+    while (suggestedKey in object) {
+      suggestedKey += i;
+      i++;
+    }
+    return suggestedKey;
+  };
+
+  const getIdsToApply = () => {
+    // Make a new set without reference
+    const idsToApply = new Set(selectedParticipants);
+    try {
+      // Always select the large video
+      const largeVideo = APP.UI.getLargeVideo();
+      if (largeVideo && largeVideo.id) {
+        // Add anyway, set won't have duplicates
+        idsToApply.add(largeVideo.id);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    return idsToApply;
+  };
+
+  const patchMethods = () => {
+    const originalsetReceiverVideoConstraintKey = generateUnusedKey(
+      "setReceiverVideoConstraint",
+      APP.conference._room
+    );
+    const originalSelectParticipantsKey = generateUnusedKey(
+      "selectParticipants",
+      APP.conference._room
+    );
+    // Back up original methods in the same object
+    APP.conference._room[originalsetReceiverVideoConstraintKey] =
+      APP.conference._room.setReceiverVideoConstraint;
+    APP.conference._room[originalSelectParticipantsKey] =
+      APP.conference._room.selectParticipants;
+
+    APP.conference._room.setReceiverVideoConstraint = () => {
+      APP.conference._room[originalsetReceiverVideoConstraintKey](
+        maxVideoHeightToReceive
+      );
+    };
+    APP.conference._room.selectParticipants = () => {
+      APP.conference._room[originalSelectParticipantsKey](
+        Array.from(getIdsToApply())
+      );
+    };
+  };
 
   const applySelectedParticipants = () => {
     try {
-      // Make a new set without reference
-      const idsToApply = new Set(selectedParticipants);
-      // Always select the large video
-      const largeVideoParticipantId = APP.UI.getLargeVideo().id;
-      // Add anyway, set won't have duplicates
-      idsToApply.add(largeVideoParticipantId);
+      const idsToApply = getIdsToApply();
 
       // Sets the maximum video size the local participant should
       // receive from selected remote participants.
@@ -39,14 +95,6 @@
           maxVideoHeightToReceive
         );
       }
-      // TODO: dominant speaker switching overrides the selectedEndpoints
-      // It will drop the resolution for the participants that are no longer selected.
-      // It will select ONLY the dominant speaker, unless we're in tile view.
-      // In tile view it will select all participants... so perhaps we do need to enforce tile view?
-      // Can we prevent dominant speaker switching? Can we prevent sending the wrong selection?
-       
-      // Dominant speaker switching works with selecting, not pinning.
-      // Can only pin 1 participant, so that's of no use either.
 
       if (
         !eqSet(idsToApply, new Set(APP.conference._room.rtc._selectedEndpoints))
@@ -54,45 +102,53 @@
         APP.conference._room.selectParticipants(Array.from(idsToApply));
       }
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
+  };
+
+  const onApiReady = () => {
+    patchMethods();
+    JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.ERROR);
+
+    bc.onmessage = e => {
+      const data = e.data;
+      if (data.select) {
+        selectedParticipants.add(data.select);
+        applySelectedParticipants();
+      } else if (data.deselect) {
+        selectedParticipants.delete(data.deselect);
+        applySelectedParticipants();
+      } else if (data.displayNameWarning) {
+        try {
+          APP.conference._room.sendPrivateTextMessage(
+            data.displayNameWarning.id,
+            data.displayNameWarning.message
+          );
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    };
+
+    // Also override by polling slowly, as backup
+    setInterval(applySelectedParticipants, 5000);
   };
 
   addEventListener("unload", () => {
     bc.close();
   });
 
-  bc.onmessage = e => {
-    const data = e.data;
-    if (data.select) {
-      selectedParticipants.add(data.select);
-      applySelectedParticipants();
-    } else if (data.deselect) {
-      selectedParticipants.delete(data.deselect);
-      applySelectedParticipants();
-    } else if (data.displayNameWarning) {
-      try {
-        APP.conference._room.sendPrivateTextMessage(
-          data.displayNameWarning.id,
-          data.displayNameWarning.message
-        );
-      } catch (e) {
-        console.log(e);
+  (function readyPoller() {
+    try {
+      if (
+        APP.conference._room.selectParticipants &&
+        APP.conference._room.setReceiverVideoConstraint
+      ) {
+        onApiReady();
       }
+    } catch (e) {
+      console.log(e);
+      setTimeout(readyPoller, 1000);
     }
-  };
-
-  // Poll quickly to override selection and resolution constraint
-  setInterval(applySelectedParticipants, 100);
-
-  // Override some styles, these selectors may stop working in the future...
-  // Fortunately these styles aren't mission critical
-  const style = document.createElement("style");
-  document.head.appendChild(style);
-  style.sheet.insertRule(
-    "body, .tOoji, #largeVideoContainer, .tile-view #largeVideoContainer {background: transparent !important; background-color: transparent !important;}"
-  );
-
-  // TODO: reduce logging
-  // JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.ERROR);
-})();
+  })();
+});
